ad60156dcc5b27559dca7fab8285b275af7883dd
eduard <gizbreht.e@yandex.ru>
2026-04-08 23:56:35 +0300

    Updates

---
+++ assets/Mallakto-BcquBe_n.js
@@ -0,0 +1,1 @@
+import{A as e,S as t,_ as n,f as r}from"./index-C0gHJoi5.js";import{n as i,t as a}from"./BuyPageContent-BZry1r96.js";var o=n({__name:`Mallakto`,setup(n){let{mallaktoGoods:o}=i();return(n,i)=>(t(),r(a,{goods:e(o)},null,8,[`goods`]))}});export{o as default};
