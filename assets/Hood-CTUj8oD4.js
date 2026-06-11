cc1acedcbaa5b0499bc706cef3f7f1d16428f84b
eduard <gizbreht.e@yandex.ru>
2026-06-11 18:49:40 +0300

    Updates

--- assets/Hood-CTUj8oD4.js
+++
@@ -1,1 +0,0 @@
-import{E as e,g as t,n,u as r,w as i,y as a}from"./index-ITHuh1-7.js";import{t as o}from"./BuyPageContent-Dxg44CmZ.js";var s=t({__name:`Hood`,setup(t){let{hoodGoods:s}=n();return(t,n)=>(a(),r(o,{goods:e(s),"onUpdate:goods":n[0]||=e=>i(s)?s.value=e:null},null,8,[`goods`]))}});export{s as default};
